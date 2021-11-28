e4c837ad49dff5b744f5417fd7b2de5cb0e7c667
vadzim <vadzimk@hotmail.com>
2021-11-27 23:00:54 -0800

    we now add two positive integers

--- test/src/link/vadzimk/math/test/AddFractionsTest.java
+++ test/src/link/vadzimk/math/test/AddFractionsTest.java
@@ -21,5 +21,10 @@ public class AddFractionsTest {
         final Fraction sum = new Fraction(0).plus(new Fraction(5));
         assertEquals(5, sum.intValue());
     }
+    @Test
+    public void positiveOperands() throws Exception{
+        Fraction sum = new Fraction(3).plus(new Fraction(4));
+        assertEquals(7, sum.intValue());
+    }
 
 }
