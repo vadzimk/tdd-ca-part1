78e44183bcc1bea284d18e6336bace76ecc4382f
vadzim <vadzimk@hotmail.com>
2021-11-28 10:28:01 -0800

    we no longer fake the denominator

--- prod/src/link/vadzimk/math/Fraction.java
+++ prod/src/link/vadzimk/math/Fraction.java
@@ -1,18 +1,21 @@
 package link.vadzimk.math;
 
 public class Fraction {
-    private int integerValue;
+    private final int integerValue;
+    private final int denominator;
 
     public Fraction(int integerValue) {
         this.integerValue = integerValue;
+        this.denominator = 1;
     }
 
     public Fraction(int numerator, int denominator) {
-
+        this.integerValue = numerator;
+        this.denominator = denominator;
     }
 
     public Fraction plus(Fraction that) {
-        return new Fraction(this.integerValue + that.integerValue);
+        return new Fraction(this.integerValue + that.integerValue, this.denominator);
     }
 
     public int intValue() {
@@ -21,10 +24,10 @@ public class Fraction {
 
 
     public int getNumerator() {
-        return 3;
+        return this.integerValue;
     }
 
     public int getDenominator() {
-        return 5;
+        return this.denominator;
     }
 }
