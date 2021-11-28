0551040d8014e4fe6f945d836c3ceb13390ce15e
vadzim <vadzimk@hotmail.com>
2021-11-28 10:53:08 -0800

    removed the integerValue field

--- prod/src/link/vadzimk/math/Fraction.java
+++ prod/src/link/vadzimk/math/Fraction.java
@@ -1,19 +1,16 @@
 package link.vadzimk.math;
 
 public class Fraction {
-    private final int integerValue;
     private final int denominator;
     private final int numerator;
 
     public Fraction(int integerValue) {
-        this.integerValue = integerValue;
         this.numerator = integerValue;
         this.denominator = 1;
     }
 
     public Fraction(int numerator, int denominator) {
         this.numerator = numerator;
-        this.integerValue = numerator;
         this.denominator = denominator;
     }
 
