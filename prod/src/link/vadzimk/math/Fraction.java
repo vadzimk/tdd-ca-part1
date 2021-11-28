154d151a4048dfd9474d20cf03741531c32e0665
vadzim <vadzimk@hotmail.com>
2021-11-28 14:04:49 -0800

    we can add fractions with different denominators as long as we don't need to reduce the result

--- prod/src/link/vadzimk/math/Fraction.java
+++ prod/src/link/vadzimk/math/Fraction.java
@@ -15,7 +15,13 @@ public class Fraction {
     }
 
     public Fraction plus(Fraction that) {
-        return new Fraction(this.numerator + that.numerator, this.denominator);
+        if (this.denominator != that.denominator) {
+            // 1   1   3*1   2*1   5
+            // - + - = --- + --- = -
+            // 2   3   3*2   2*3   6
+            return new Fraction(this.numerator * that.denominator + that.numerator * this.denominator, this.denominator * that.denominator);
+        } else
+            return new Fraction(this.numerator + that.numerator, this.denominator);
     }
 
 
--- test/src/link/vadzimk/math/test/AddFractionsTest.java
+++ test/src/link/vadzimk/math/test/AddFractionsTest.java
@@ -11,30 +11,41 @@ public class AddFractionsTest {
         Fraction sum = new Fraction(0).plus(new Fraction(0));
         assertEquals(new Fraction(0), sum);
     }
+
     @Test
     public void nonZeroPlusZero() {
         final Fraction sum = new Fraction(3).plus(new Fraction(0));
         assertEquals(new Fraction(3), sum);
     }
+
     @Test
     public void zeroPlusNonZero() {
         final Fraction sum = new Fraction(0).plus(new Fraction(5));
         assertEquals(new Fraction(5), sum);
     }
+
     @Test
     public void positiveOperands() {
-        final Fraction  sum = new Fraction(3).plus(new Fraction(4));
+        final Fraction sum = new Fraction(3).plus(new Fraction(4));
         assertEquals(new Fraction(7), sum);
     }
+
     @Test
-    public void negativeInputsNegativeOutput(){
+    public void negativeInputsNegativeOutput() {
         final Fraction sum = new Fraction(-3).plus(new Fraction(1));
         assertEquals(new Fraction(-2), sum);
     }
+
     @Test
-    public void nonTrivialCommonDenominator(){
-        final Fraction sum = new Fraction(1,5).plus(new Fraction(2,5));
-        assertEquals(new Fraction(3,5), sum);
+    public void nonTrivialCommonDenominator() {
+        final Fraction sum = new Fraction(1, 5).plus(new Fraction(2, 5));
+        assertEquals(new Fraction(3, 5), sum);
     }
 
+    @Test
+    public void differentDenominatorsWithoutReducing() {
+        assertEquals(
+                new Fraction(5, 6),
+                new Fraction(1, 2).plus(new Fraction(1, 3)));
+    }
 }
