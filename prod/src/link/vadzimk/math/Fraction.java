0d3a91f0f1a439271f38236cbf6591323c37200d
vadzim <vadzimk@hotmail.com>
2021-11-27 22:47:57 -0800

    we now add 0 to non-zero

--- prod/src/link/vadzimk/math/Fraction.java
+++ prod/src/link/vadzimk/math/Fraction.java
@@ -3,15 +3,18 @@ package link.vadzimk.math;
 public class Fraction {
     private final int integerValue;
 
-    public Fraction(int integerValue){
+    public Fraction(int integerValue) {
         this.integerValue = integerValue;
     }
 
-    public Fraction plus(Fraction that){
-        return this;
+    public Fraction plus(Fraction that) {
+        if (that.integerValue != 0)
+            return that;
+        else
+            return this;
     }
 
-    public int intValue(){
+    public int intValue() {
         return integerValue;
     }
 }
