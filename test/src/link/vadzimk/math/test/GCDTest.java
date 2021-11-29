aa82ec44e3a68525e28e61d1f7e203eaae104c04
vadzim <vadzimk@hotmail.com>
2021-11-28 19:27:56 -0800

    gcd now returns abs value

--- test/src/link/vadzimk/math/test/GCDTest.java
+++ test/src/link/vadzimk/math/test/GCDTest.java
@@ -6,9 +6,28 @@ import static org.junit.Assert.assertEquals;
 
 public class GCDTest {
     @Test
-    public void oneAndOne() {
+    public void reflexive() {
         assertEquals(1, gcd(1, 1));
+        assertEquals(2, gcd(2,2));
+        assertEquals(1, gcd(-1, -1));
 
+    }
+
+    @Test
+    public void relativelyPrime() {
+        assertEquals(1, gcd(2,3));
+        assertEquals(1, gcd(4,7));
+        assertEquals(1, gcd(-2,-3));
+    }
+
+    @Test
+    public void oneMultipleOfOther() {
+        assertEquals(3, gcd(3,9));
+    }
+
+    @Test
+    public void commonFactor() {
+        assertEquals(2, gcd(6,8));
     }
 
     private int gcd(int a, int b) {
@@ -17,8 +36,7 @@ public class GCDTest {
             b = a % t;
             a = t;
         }
-        return a;
-
+        return Math.abs(a);
     }
 }
 
